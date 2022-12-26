53ac31249d49ce15188cc39985c176ee2d497d0b
Goran Markovic <goranmarkovic2008@gmail.com>
2022-12-26 19:37:06 +0100

    added header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from"./Header.module.css";
+import mealsImage from "../Assets/meals.jpg";
+function Header(props) {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of food" />
+      </div>
+    </>
+  );
+}
+
+export default Header;
