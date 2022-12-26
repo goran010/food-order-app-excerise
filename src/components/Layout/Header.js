7315b9462c6f6866be4f42c0ce448501f18f8bac
Goran Markovic <goranmarkovic2008@gmail.com>
2022-12-26 20:39:54 +0100

    added HeaderCartButton
    added HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
-import classes from"./Header.module.css";
+import classes from "./Header.module.css";
 import mealsImage from "../Assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 function Header(props) {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
+        <HeaderCartButton />
         <button>Cart</button>
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of food" />
       </div>
     </>
