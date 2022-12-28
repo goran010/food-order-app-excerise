99cfeed61214aee0bdc0c455ccfda810cd32b3f8
Goran Markovic <goranmarkovic2008@gmail.com>
2022-12-28 21:35:53 +0100

    added functionality on badge in HeaderCartButton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContex from "../../store/cart-contex";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContex);
+  const numberOfItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 }
