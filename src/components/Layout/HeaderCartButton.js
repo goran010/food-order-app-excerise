4da9e5878895e6301f397f1d464c3eb3fada48a8
Goran Markovic <goranmarkovic2008@gmail.com>
2023-01-09 18:03:25 +0100

    typo

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContex from "../../store/cart-contex";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+  const [btnIsHighlighted, setBtnisHighlighted] = useState(false);
   const cartCtx = useContext(CartContex);
   const numberOfItems = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnisHighlighted(true);
+    let timer = setTimeout(() => {
+      setBtnisHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
